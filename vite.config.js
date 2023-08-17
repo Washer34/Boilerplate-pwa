f5a244d09f32036e6deb5fb55ff6231cac7ef6c4
Antoine Paturel <antoine.paturel34@free.fr>
2023-08-17 12:03:10 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,38 @@ export default defineConfig({
         background_color: '#90caf9',
         icons: [
           {
-            src: '/pwa-icon.png',
+            src: '/twotter-icon-256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/twotter-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/twotter-icon-192.png',
             sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/twotter-icon-152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: '/twotter-icon-144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: '/twotter-icon-128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: '/twotter-icon-16.png',
+            sizes: '16x16',
             type: 'image/png',
           },
           
